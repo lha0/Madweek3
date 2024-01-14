e9981d6606428782a9bb488127cf7f2694ef7975
hemin0806 <hemin0806@naver.com>
2024-01-14 15:55:01 +0900

    소켓 create & 소켓 뷰모델 정의

--- app/src/main/java/com/example/madweek3/GameActivity.kt
+++ app/src/main/java/com/example/madweek3/GameActivity.kt
@@ -12,34 +12,10 @@ import java.net.URISyntaxException
 
 class GameActivity : AppCompatActivity() {
 
-    private lateinit var socketViewModel: SocketViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
 
-        // SocketViewModel 초기화
-        socketViewModel = ViewModelProvider(this).get(SocketViewModel::class.java)
-
-        // 소켓 생성 및 ViewModel에 설정
-        createSocket()
-
     }
 
-    private fun createSocket() {
-        GlobalScope.launch(Dispatchers.IO) {
-            try {
-                // 소켓 생성
-                val socket = IO.socket("http:/54.180.125.145:5000/")
-
-                // ViewModel에 소켓 설정
-                socketViewModel.socket = socket
-
-                // 통신 시작 등의 작업 수행
-                // ...
-
-            } catch (e: URISyntaxException) {
-                e.printStackTrace()
-            }
-        }
-    }
 }
--- app/src/main/java/com/example/madweek3/MainActivity.kt
+++ app/src/main/java/com/example/madweek3/MainActivity.kt
@@ -2,15 +2,50 @@ package com.example.madweek3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import io.socket.client.IO
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import java.net.URISyntaxException
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var socketViewModel: SocketViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        // SocketViewModel 초기화
+        socketViewModel = ViewModelProvider(this).get(SocketViewModel::class.java)
+
+        // 소켓 생성 및 ViewModel에 설정
+        createSocket()
+
 
         supportFragmentManager.beginTransaction()
             .replace(R.id.main_container, MainFragment())
             .commit()
 
     }
+
+    private fun createSocket() {
+        GlobalScope.launch(Dispatchers.IO) {
+            try {
+                // 소켓 생성
+                val socket = IO.socket("http:/54.180.125.145:5000/")
+
+                // ViewModel에 소켓 설정
+                socketViewModel.socket = socket
+
+                // 통신 시작 등의 작업 수행
+                // ...
+
+            } catch (e: URISyntaxException) {
+                e.printStackTrace()
+            }
+        }
+    }
+
 }
